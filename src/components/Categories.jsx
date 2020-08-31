3512d93467f311383a53bd03861b04266c5b59bc
José Eduardo Vinagre de Dios <eduardovinagre@gmail.com>
2020-08-31 12:10:09 -0500

    Se agrega custom hook

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,7 +3,7 @@ import '../assets/styles/components/categories.scss';
 
 const Categories = ({ children, title }) => (
   <div className="categories">
-    <h3 class="categories__title">{title}</h3>
+    <h3 className="categories__title">{title}</h3>
     {children}
   </div>
 );

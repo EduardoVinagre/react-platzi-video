ff8e5975468091c31dee5ffa4c769dafa5fbec0c
José Eduardo Vinagre de Dios <eduardovinagre@gmail.com>
2020-08-31 08:31:39 -0500

    Se integra el diseño de la pagina web a React

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../assets/styles/components/carousel.scss';
+
+const Carousel = ({ children }) => (
+  <section class="carousel">
+    <div class="carousel__container">{children}</div>
+  </section>
+);
+
+export default Carousel;
